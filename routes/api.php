bc67820d175b52107564d8ab11bb163011dae229
ahnuf <ahnufkarimchowdhury@gmail.com>
2024-12-27 22:22:33 +0600

    AuthController & AdminMiddleware Done!

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/admin', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum',AdminMiddleware::class);
 
 Route::post('/login',[AuthController::class,'login'])->name('login');
 
